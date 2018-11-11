140760718e6aba976aa39a98fc0698e2fe399707
uopsdod <uopsdod@gmail.com>
2018-11-11 17:13:56 +0800

    first commit

---
+++ CodeCommit_project/src/main/java/CodeCommit_project/CodeCommit_project/App.java
@@ -0,0 +1,13 @@
+package CodeCommit_project.CodeCommit_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ CodeCommit_project/src/main/java/CodeCommit_project/CodeCommit_project/MessageUtil.java
@@ -0,0 +1,20 @@
+package CodeCommit_project.CodeCommit_project;
+
+public class MessageUtil {
+	  private String message;
+
+	  public MessageUtil(String message) {
+	    this.message = message;
+	  }
+
+	  public String printMessage() {
+	    System.out.println(message);
+	    return message;
+	  }
+
+	  public String salutationMessage() {
+	    message = "Hi!" + message;
+	    System.out.println(message);
+	    return message;
+	  }
+	}
